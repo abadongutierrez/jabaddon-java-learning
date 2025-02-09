fbf431208c4fbd8404f7ef2f1fc47dd9c1f964c3
abadongutierrez <abadon.gutierrez@gmail.com>
2025-02-08 23:21:11 -0600

    exploring polymorphism vs sealed classes

---
+++ polymorphism_n_alternatives/src/main/java/com/jabaddon/practices/Application.java
@@ -0,0 +1,8 @@
+package com.jabaddon.practices;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ polymorphism_n_alternatives/src/test/java/com/jabaddon/practices/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jabaddon.practices;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
